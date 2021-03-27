8f583ac63d3f2d0cef82ca74f99985bc14d39c46
Supreme-YS <dudtjr1225@gmail.com>
2021-03-27 19:20:02 +0900

    java

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype{
+	public static void main(String[] args) {
+		
+		System.out.println(6);      // Number
+		System.out.println("six");  // String
+		System.out.println("6");    // String
+		System.out.println(5+5);    // 10
+		System.out.println("6"+"6");// 66 문자와 문자를 붙여준다.
+		System.out.println(6*6);    // 36
+//      System.out.println("6"*"6");// error
+		
+		System.out.println("1111".length()); //4
+//		System.out.println(1111.length()); //error
+		
+	}
+}
