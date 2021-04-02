0f44e8223582eac179358ef05646adfeaca041ae
Supreme-YS <dudtjr1225@gmail.com>
2021-04-02 20:26:15 +0900

    java

---
+++ Practice/LoginDemo.java
@@ -0,0 +1,13 @@
+
+public class LoginDemo {
+
+	public static void main(String[] args) {
+		
+		String id = args[0];
+		if(id.equals("supreme")) {
+			System.out.println("right");}
+		else {
+			System.out.println("wrong");
+		}
+	}
+}
