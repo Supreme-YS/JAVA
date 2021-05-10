1a5c715c5b850981563c630c61e9953acf3531f9
Sim Youngseok <46801877+Supreme-YS@users.noreply.github.com>
2021-05-10 17:41:46 +0900

    update java code

--- java_workspace/src/main/java/lec13/ChildClass.java
+++ java_workspace/src/main/java/lec13/ChildClass.java
@@ -10,6 +10,13 @@ public class ChildClass {
         System.out.println("-- ChildClass Constructor --");
     }
 
+    public void setInfo(String n, String g, int a) {
+        System.out.println("-- setInfo() START --");
+        name = n;
+        gender = g;
+        age = a;
+    }
+
     public void getInfo() {
         System.out.println("-- getInfo() START --");
         System.out.println("name :" + name);
--- java_workspace/src/main/java/lec13/MainClass.java
+++ java_workspace/src/main/java/lec13/MainClass.java
@@ -4,9 +4,6 @@ public class MainClass {
 
     public static void main(String[] args) {
         ChildClass child1 = new ChildClass();
-        child1.name = "홍길동";
-        child1.gender = "M";
-        child1.age =  20;
-        child1.getInfo();
+        child1.setInfo("ys", "male", 20);
     }
 }
