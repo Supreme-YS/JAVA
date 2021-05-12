b96233997f2e3a8ecb7e0f1f51c816323c0255ba
Sim Youngseok <46801877+Supreme-YS@users.noreply.github.com>
2021-05-12 18:32:43 +0900

    java TIL
    상속

---
+++ java_workspace/src/main/java/lec19/ChildClass.java
@@ -0,0 +1,13 @@
+package lec19;
+
+public class ChildClass extends ParentClass {
+
+    public ChildClass() {
+        System.out.println("--ChildClass Constructor--");
+    }
+
+    public void childFun() {
+        System.out.println("--childFun() start--");
+    }
+
+}
