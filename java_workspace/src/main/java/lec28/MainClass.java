9650ea26fb5e5fd600f36220dbd08588441fcd23
Sim Youngseok <46801877+Supreme-YS@users.noreply.github.com>
2021-05-18 23:27:30 +0900

    Java TIL

--- java_workspace/src/main/java/lec28/MainClass.java
+++ java_workspace/src/main/java/lec28/MainClass.java
@@ -11,10 +11,10 @@ public class MainClass {
 
         try {
             serverSocket = new ServerSocket(9000);
-            System.out.println("클라이언트 맞을 준비 완료");
+            System.out.println("클라이언트 수신 준비 완료");
 
             socket = serverSocket.accept();
-            System.out.println("클라이언트 연결");
+            System.out.println("클라이언트 연결 완료");
             System.out.println("socket : " + socket);
 
         } catch (Exception e) {
