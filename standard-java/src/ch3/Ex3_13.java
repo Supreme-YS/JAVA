56d03df9706d7a3693245dcc4cb3f3ba6f1a0cba
Supreme-YS <dudtjr1225@gmail.com>
2021-11-29 18:52:09 +0900

    ch3

---
+++ standard-java/src/ch3/Ex3_13.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class Ex3_13 {
+    public static void main(String[] args) {
+
+        System.out.println(-10%8);
+        System.out.println(10%-8); // 나누는 수로 음수 허용, 부호는 무시 된다. 결과는 음수의 절대값으로 나눈 나머지와 결과 값이 같다.
+        System.out.println(-10%-8);
+    }
+}
