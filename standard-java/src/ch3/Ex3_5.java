41cf02fa427d0863efcf2fe6079e28bcfd4a5712
Supreme-YS <dudtjr1225@gmail.com>
2021-11-29 17:23:12 +0900

    Casting

---
+++ standard-java/src/ch3/Ex3_5.java
@@ -0,0 +1,11 @@
+package ch3;
+
+public class Ex3_5 {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int) d;
+        System.out.println("score = " + score);
+        System.out.println("d = " + d);
+        // 형 변환 이후에도 피연산자(operand)는 아무런 변화가 없다.
+    }
+}
