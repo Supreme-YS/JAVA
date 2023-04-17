3a7539f13407870f6869fc99d7200f799bf06768
Supreme-YS <dudtjr1225@gmail.com>
2023-04-17 14:15:19 +0900

    Add Higher-Order-Functions

--- java8/java8to11/src/main/java/me/supremeys/Foo.java
+++ java8/java8to11/src/main/java/me/supremeys/Foo.java
@@ -14,7 +14,17 @@ public class Foo {
         Function<Integer, Integer> plus20 = (i) -> {
             return i + 20;
         };
+        System.out.println(plus20.apply(10));
         // 람다식 표현 - 2
         Function<Integer, Integer> plus30 = (i) -> i+20;
+        System.out.println(plus30.apply(30));
+
+        // Higher-Order Functions
+        Function<Integer, Integer> minusOne = (i) -> i-1;
+        Function<Integer, Integer> multiplyTwo = (i) -> i*2;
+
+        Function<Integer, Integer> multiplyTwoAndMinusOne = minusOne.compose(multiplyTwo);
+        System.out.println(multiplyTwoAndMinusOne.apply(10));
+
     }
 }
